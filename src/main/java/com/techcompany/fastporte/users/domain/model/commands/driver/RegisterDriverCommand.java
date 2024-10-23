b23b11959a16315a53ec8a39ae73a3c56e3bfbb6
Anthony Botello <89035933+AnthonyBotello4@users.noreply.github.com>
2024-10-23 10:32:03 -0500

    feat: add users commands

---
+++ src/main/java/com/techcompany/fastporte/users/domain/model/commands/driver/RegisterDriverCommand.java
@@ -0,0 +1,14 @@
+package com.techcompany.fastporte.users.domain.model.commands.driver;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record RegisterDriverCommand(
+    @NotBlank(message = "The name is required") String name,
+    @NotBlank(message = "The first last name is required") String firstLastName,
+    @NotBlank(message = "The second last name is required") String secondLastName,
+    @NotBlank(message = "The email is required") String email,
+    @NotBlank(message = "The username is required") String username,
+    @NotBlank(message = "The password is required") String password
+) {
+
+}
