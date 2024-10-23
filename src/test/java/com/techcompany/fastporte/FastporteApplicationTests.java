0ab8e6624ecc93029e2552ed5df9757f261fddc5
Anthony Botello <89035933+AnthonyBotello4@users.noreply.github.com>
2024-10-23 10:12:04 -0500

    feat: base project

---
+++ src/test/java/com/techcompany/fastporte/FastporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techcompany.fastporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastporteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
