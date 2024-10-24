5494a8afccf20b0757195ad4fc69139d575230a8
Anthony Botello <89035933+AnthonyBotello4@users.noreply.github.com>
2024-10-24 08:31:28 -0500

    feat(shared): app configuration

---
+++ src/main/java/com/techcompany/fastporte/shared/utils/EnvironmentConstants.java
@@ -0,0 +1,9 @@
+package com.techcompany.fastporte.shared.utils;
+
+public interface EnvironmentConstants {
+
+    String ENV_PROD_URL = "https://link-de-despligue";
+    String ENV_DEV_URL = "http://localhost:8080";
+
+    String CURRENT_ENV_URL = ENV_DEV_URL;
+}
---
+++ src/main/java/com/techcompany/fastporte/shared/utils/ValidationMessageConstants.java
@@ -0,0 +1,13 @@
+package com.techcompany.fastporte.shared.utils;
+
+public interface ValidationMessageConstants {
+
+    Long LENGTH_NAME = 50L;
+    Long LENGTH_LAST_NAME = 50L;
+    int LENGTH_USERNAME = 20;
+
+    String OBLIGATORY_FIELD = "Este campo es obligatorio";
+    String BLANK_FIELD = "Este campo no puede estar vacío";
+    String NULL_FIELD = "Este campo no puede ser nulo";
+
+}
