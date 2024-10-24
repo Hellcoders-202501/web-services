ec60e2d9bc8c4bc03691f0f428900efdeb120185
Anthony Botello <89035933+AnthonyBotello4@users.noreply.github.com>
2024-10-23 21:09:51 -0500

    feat(trips): add repositories

---
+++ src/main/java/com/techcompany/fastporte/trips/infrastructure/persistence/jpa/TripRepository.java
@@ -0,0 +1,16 @@
+package com.techcompany.fastporte.trips.infrastructure.persistence.jpa;
+
+import com.techcompany.fastporte.trips.domain.model.aggregates.entities.Trip;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TripRepository extends JpaRepository<Trip, Long> {
+
+    List<Trip> findAllByDriverId(Long id);
+    List<Trip> findAllByDriverIdAndStatus_Id(Long driverId, Long statusId);
+    List<Trip> findAllByDriverIdIn(List<Long> driverIds);
+    List<Trip> findAllByStatus_Id (Long statusId);
+}
