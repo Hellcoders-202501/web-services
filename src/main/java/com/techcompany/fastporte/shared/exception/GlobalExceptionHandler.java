5395a8893d901bcf5ece09f6c1495116b58f8b9e
Anthony Botello <89035933+AnthonyBotello4@users.noreply.github.com>
2024-10-29 17:39:03 -0500

    feat(users): global exception handler added

---
+++ src/main/java/com/techcompany/fastporte/shared/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.techcompany.fastporte.shared.exception;
+
+import com.techcompany.fastporte.users.domain.model.exceptions.SupervisorNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+    @ExceptionHandler(SupervisorNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleSupervisorNotFoundException(SupervisorNotFoundException ex) {
+        Map<String, String> errors = new HashMap<>();
+        errors.put("message", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errors);
+    }
+}
