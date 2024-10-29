ca2f975231bffda4538d3d3c61bb137e91596d11
JosArr <josuedavidarrunategui@gmail.com>
2024-10-29 14:00:51 -0500

    feat(security): add Assemblers from Resource

---
+++ src/main/java/com/techcompany/fastporte/security/interfaces/rest/transform/fromResource/CreateSafetyThresholdCommandFromResourceAssembler.java
@@ -0,0 +1,17 @@
+package com.techcompany.fastporte.security.interfaces.rest.transform.fromResource;
+
+import com.techcompany.fastporte.security.domain.model.commands.CreateSafetyThresholdCommand;
+import com.techcompany.fastporte.security.interfaces.rest.resources.CreateSafetyThresholdResource;
+
+public class CreateSafetyThresholdCommandFromResourceAssembler {
+
+    public static CreateSafetyThresholdCommand toCommandFromResource(CreateSafetyThresholdResource resource) {
+        return new CreateSafetyThresholdCommand(
+                resource.sensorTypeId(),
+                resource.maxThreshold(),
+                resource.minThreshold(),
+                resource.tripId(),
+                resource.supervisorId()
+        );
+    }
+}
