a61196d5e4e7cab797a58d41f855abd416a8951d
Anthony Botello <89035933+AnthonyBotello4@users.noreply.github.com>
2024-10-31 00:01:55 -0500

    fix: update base url for deployment

--- src/main/java/com/techcompany/fastporte/shared/utils/EnvironmentConstants.java
+++ src/main/java/com/techcompany/fastporte/shared/utils/EnvironmentConstants.java
@@ -2,8 +2,8 @@ package com.techcompany.fastporte.shared.utils;
 
 public interface EnvironmentConstants {
 
-    String ENV_PROD_URL = "https://link-de-despligue";
+    String ENV_PROD_URL = "https://fastporte-cloud-api.azurewebsites.net";
     String ENV_DEV_URL = "http://localhost:8080";
 
-    String CURRENT_ENV_URL = ENV_DEV_URL;
+    String CURRENT_ENV_URL = ENV_PROD_URL;
 }
