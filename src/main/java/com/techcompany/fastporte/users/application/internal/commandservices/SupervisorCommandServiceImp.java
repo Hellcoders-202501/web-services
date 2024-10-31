ffcffa0984bfc767b7bae813caee3ca26823dd2d
Anthony Botello <89035933+AnthonyBotello4@users.noreply.github.com>
2024-10-30 23:30:29 -0500

    fix(users): verify if supervisor email exists fixed

--- src/main/java/com/techcompany/fastporte/users/application/internal/commandservices/SupervisorCommandServiceImp.java
+++ src/main/java/com/techcompany/fastporte/users/application/internal/commandservices/SupervisorCommandServiceImp.java
@@ -1,15 +1,15 @@
 package com.techcompany.fastporte.users.application.internal.commandservices;
 
-import com.techcompany.fastporte.users.domain.model.aggregates.entities.Driver;
 import com.techcompany.fastporte.users.domain.model.aggregates.entities.Role;
 import com.techcompany.fastporte.users.domain.model.aggregates.entities.Supervisor;
 import com.techcompany.fastporte.users.domain.model.aggregates.entities.User;
 import com.techcompany.fastporte.users.domain.model.aggregates.enums.RoleName;
-import com.techcompany.fastporte.users.domain.model.commands.driver.UpdateDriverInformationCommand;
 import com.techcompany.fastporte.users.domain.model.commands.supervisor.DeleteSupervisorCommand;
 import com.techcompany.fastporte.users.domain.model.commands.supervisor.RegisterSupervisorCommand;
 import com.techcompany.fastporte.users.domain.model.commands.supervisor.UpdateSupervisorInformationCommand;
 import com.techcompany.fastporte.users.domain.model.exceptions.DriverNotFoundException;
+import com.techcompany.fastporte.users.domain.model.exceptions.EmailAlreadyExistsException;
+import com.techcompany.fastporte.users.domain.model.exceptions.RoleNotFoundException;
 import com.techcompany.fastporte.users.domain.model.exceptions.SupervisorNotFoundException;
 import com.techcompany.fastporte.users.domain.services.supervisor.SupervisorCommandService;
 import com.techcompany.fastporte.users.infrastructure.persistence.jpa.RoleRepository;
@@ -40,6 +40,13 @@ public class SupervisorCommandServiceImp implements SupervisorCommandService {
 
     @Override
     public Optional<Supervisor> handle(RegisterSupervisorCommand command) {
+
+        //Buscar si el username/*email* existe
+        if (userRepository.existsByEmail(command.email())) {
+            System.out.println("Email already exists");
+            throw new EmailAlreadyExistsException(command.email());
+        }
+
         // Mapear el objeto SupervisorRegisterDto a un objeto Supervisor
         Supervisor supervisor = new Supervisor(command);
 
@@ -48,7 +55,7 @@ public class SupervisorCommandServiceImp implements SupervisorCommandService {
 
         // Recuperar el rol desde la base de datos
         Role supervisorRole = roleRepository.findByRoleName(RoleName.ROLE_SUPERVISOR)
-                .orElseThrow(() -> new RuntimeException("Error: El rol ROLE_SUPERVISOR no existe en la base de datos"));
+                .orElseThrow(() -> new RoleNotFoundException(RoleName.ROLE_SUPERVISOR.toString()));
 
         // Asignar el rol al usuario
         user.setRoles(Set.of(supervisorRole));
