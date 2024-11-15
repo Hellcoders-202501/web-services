e0938ef3d221e3cd3d636e2559ed359481439f58
Anthony Botello <89035933+AnthonyBotello4@users.noreply.github.com>
2024-11-15 13:17:12 -0500

    fix: trip fields validation fixed

--- src/main/java/com/techcompany/fastporte/trips/interfaces/rest/resources/CreateTripResource.java
+++ src/main/java/com/techcompany/fastporte/trips/interfaces/rest/resources/CreateTripResource.java
@@ -1,16 +1,18 @@
 package com.techcompany.fastporte.trips.interfaces.rest.resources;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record CreateTripResource(
-        Long driverId,
-        Long supervisorId,
-        String origin,
-        String destination,
-        String type,
-        String amount,
-        String weight,
-        String date,
-        String startTime,
-        String endTime,
+        @NotBlank(message = "Driver ID is required") Long driverId,
+        @NotBlank(message = "Supervisor ID is required") Long supervisorId,
+        @NotBlank(message = "Origin is required") String origin,
+        @NotBlank(message = "Destination is required") String destination,
+        @NotBlank(message = "Type is required") String type,
+        @NotBlank(message = "Amount is required") String amount,
+        @NotBlank(message = "Weight is required") String weight,
+        @NotBlank(message = "Date is required") String date,
+        @NotBlank(message = "Start time is required") String startTime,
+        @NotBlank(message = "End time is required") String endTime,
         String subject,
         String description
 ) {
--- src/main/java/com/techcompany/fastporte/trips/interfaces/rest/transform/fromEntity/TripInformationResourceFromEntityAssembler.java
+++ src/main/java/com/techcompany/fastporte/trips/interfaces/rest/transform/fromEntity/TripInformationResourceFromEntityAssembler.java
@@ -6,20 +6,26 @@ import com.techcompany.fastporte.trips.interfaces.rest.resources.TripInformation
 public class TripInformationResourceFromEntityAssembler {
 
     public static TripInformationResource toResourceFromEntity(Trip trip){
+
+        String driverName = trip.getDriver().getUser().getName() + " " + trip.getDriver().getUser().getFirstLastName() + " " + trip.getDriver().getUser().getSecondLastName();
+        String supervisorName = trip.getSupervisor().getUser().getName() + " " + trip.getSupervisor().getUser().getFirstLastName() + " " + trip.getSupervisor().getUser().getSecondLastName();
+
+        String date = trip.getDate() == null ? "" : trip.getDate().toString();
+
         return new TripInformationResource(
                 trip.getId(),
                 trip.getDriver().getId(),
-                trip.getDriver().getUser().getName() + " " + trip.getDriver().getUser().getFirstLastName() + " " + trip.getDriver().getUser().getSecondLastName(),
+                driverName,
                 trip.getDriver().getUser().getPhone(),
                 trip.getSupervisor().getId(),
-                trip.getSupervisor().getUser().getName() + " " + trip.getSupervisor().getUser().getFirstLastName() + " " + trip.getSupervisor().getUser().getSecondLastName(),
+                supervisorName,
                 trip.getSupervisor().getUser().getPhone(),
                 trip.getOrigin(),
                 trip.getDestination(),
                 trip.getType(),
                 trip.getAmount(),
                 trip.getWeight(),
-                trip.getDate().toString(),
+                date,
                 trip.getStartTime(),
                 trip.getEndTime(),
                 trip.getSubject(),
