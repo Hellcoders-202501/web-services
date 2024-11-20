86cfde00657f3ebf7119bcc3b77c32dd0391b113
Anthony Botello <89035933+AnthonyBotello4@users.noreply.github.com>
2024-11-20 01:36:25 -0500

    fix: alert resource fixed to receive timestamp

--- src/main/java/com/techcompany/fastporte/security/interfaces/rest/transform/fromResource/CreateAlertCommandFromResourceAssembler.java
+++ src/main/java/com/techcompany/fastporte/security/interfaces/rest/transform/fromResource/CreateAlertCommandFromResourceAssembler.java
@@ -4,18 +4,25 @@ import com.techcompany.fastporte.security.domain.model.commands.CreateAlertComma
 import com.techcompany.fastporte.security.interfaces.rest.resources.CreateAlertResource;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 public class CreateAlertCommandFromResourceAssembler {
 
     public static CreateAlertCommand fromResource(CreateAlertResource resource) {
 
         //Parsear el timestamp a LocalDateTime
-        LocalDateTime timestamp = LocalDateTime.parse(resource.timestamp());
+        //LocalDateTime timestamp = LocalDateTime.parse(resource.timestamp());
+
+        // Obtén la zona horaria de Perú
+        ZoneId peruZone = ZoneId.of("America/Lima");
+        ZonedDateTime peruTime = ZonedDateTime.now(peruZone);
+        LocalDateTime now = peruTime.toLocalDateTime();
 
         return new CreateAlertCommand(
                 resource.sensorType(),
                 resource.value(),
-                timestamp,
+                now,
                 resource.tripId()
         );
     }
