7c2a12a723f9f0ff15b4b859c1d63ffa5cb21f3f
Anthony Botello <89035933+AnthonyBotello4@users.noreply.github.com>
2025-06-16 23:46:33 -0500

    chore: specify json content

--- src/main/java/com/techcompany/fastporte/users/infrastructure/auth/springsecurity/SecurityConfig.java
+++ src/main/java/com/techcompany/fastporte/users/infrastructure/auth/springsecurity/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
     private static final String[] WHITE_LIST_URL = {
             "/api/v1/auth",
             "/api/v1/drivers/**",
+            "/api/v1/drivers/experience",
             "/api/v1/clients/**",
             "/v3/api-docs/**",
             "/swagger-ui/**",
